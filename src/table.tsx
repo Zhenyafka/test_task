e4a4fd885964040a7554b3747bf413bb6e1235c1
zhenyafka <zhenyafka163@gmail.com>
2024-03-22 16:19:50 +0400

    Fixed pagination

--- src/table.tsx
+++ src/table.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import {Paper, Table, TableBody, TableRow, TableCell, TableContainer, TableHead, TablePagination} from "@mui/material";
 
 
 interface Column {
@@ -17,7 +10,7 @@ interface Column {
     format?: (value: number) => string;
 }
 
-const columns: readonly Column[] = [
+const columns: Column[] = [
     { id: 'name', label: 'Name', minWidth: 170 },
     { id: 'price', label: 'Price', minWidth: 100, format: (value: number) => value.toFixed(2), },
     {id: 'brand', label: 'Brand Name', minWidth: 170, }
@@ -58,26 +51,20 @@ const rows = [
     createData('eryy', 300, 'zxcvbn'),
     createData('erty', 100, 'qwerty'),
     createData('io[', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
-    createData('[]p[]]', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
+    createData('[]p[]]', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
     createData('cxvvxcbv', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
     createData('zxc', 300, 'zxcvbn'),
     createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
     createData('zxc', 300, 'zxcvbn'),
     createData('qwe', 100, 'qwerty'),
@@ -85,32 +72,36 @@ const rows = [
     createData('zxc', 300, 'zxcvbn'),
     createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
-    createData('asd', 200, 'asdfgh'),
-    createData('zxc', 300, 'zxcvbn'),createData('qwe', 100, 'qwerty'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
     createData('zxc', 300, 'zxcvbn'),
     createData('qwe', 100, 'qwerty'),
     createData('asd', 200, 'asdfgh'),
     createData('zxc', 300, 'zxcvbn'),
-
-
-
-
-
-
-
-
-
-
-
-
-
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
+    createData('qwe', 100, 'qwerty'),
+    createData('asd', 200, 'asdfgh'),
+    createData('zxc', 300, 'zxcvbn'),
 ];
 
 export const ProductTable = () => {
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [rowsPerPage, setRowsPerPage] = React.useState(50);
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -138,23 +129,23 @@ export const ProductTable = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
+                        {
+                            rows
+                                .slice(page, rowsPerPage)
+                                .map((row) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1}
-                                              key={row.name}
-                                    >
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    {column.format && typeof value === 'number'
-                                                        ? column.format(value)
-                                                        : value}
-                                                </TableCell>
-                                            );
-                                        })}
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
+                                        {
+                                            columns.map((column) => {
+                                                const value = row[column.id];
+                                                return (
+                                                    <TableCell  key={column.id} align={column.align}>
+                                                        {column.format && typeof value === 'number'
+                                                            ? column.format(value)
+                                                            : value}
+                                                    </TableCell>
+                                                );
+                                            })}
                                     </TableRow>
                                 );
                             })}
